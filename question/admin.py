e9ed0fe6356f662baf35c79b07899902a59da577
MarinaIgorevna4 <marinatsykunenko97@gmail.com>
2022-03-29 16:08:17 +0300

    Iteration 2. Django Admin Improvements

--- question/admin.py
+++ question/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Question)
+# admin.site.register(models.Question)
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('main_question', 'publish_question')
+    search_fields = ('main_question', )
+    prepopulated_fields = {'slug': ('publish_question', )}
