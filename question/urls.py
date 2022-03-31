ebcacb947de0e6253f88c452421ab7da92aa4ad6
MarinaIgorevna4 <marinatsykunenko97@gmail.com>
2022-03-31 13:26:06 +0300

    Iteration 11. LoginRequiredMixin and login/logout pages

--- question/urls.py
+++ question/urls.py
@@ -5,11 +5,10 @@ from django.contrib.auth import views as auth_views
 app_name = 'question'
 urlpatterns = [
     path('', views.question_today, name='question_today'),
-    path('login/', views.custom_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('login/', views.custom_login, name='login'),
     # path('all-questions/', views.all_questions, name='all_questions'),
     path('all-questions/', views.QuestionListView.as_view(), name='all_questions'),
     path('<slug:slug>/', views.discussion_question, name='discussion_question'),
-
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
